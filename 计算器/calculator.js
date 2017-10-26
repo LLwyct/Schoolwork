e0b7df313e240021098a2a7148214f5399bac6e8
liwenchi123000 <787552171@qq.com>
2017-10-26 13:08:05 +0800

    Update calculator.js

--- 计算器/calculator.js
+++ 计算器/calculator.js
@@ -2,25 +2,25 @@ var expression = document.getElementById('expression')
 var keys = document.getElementsByName('button')
 
 for(var i=0; i<keys.length; i++) {
-	keys[i].addEventListener('click', function() {
+    keys[i].addEventListener('click', function() {
 	if(this.value == '=') {
-		var exp = (expression.value).replace(/√/g,'Math.sqrt')
-        exp = exp.replace(/\^/g,'**')
-        try {
-            expression.value = eval(exp)
-        } catch (EvalError){
-            expression.value = 'error'
+	    var exp = (expression.value).replace(/√/g,'Math.sqrt')
+            exp = exp.replace(/\^/g,'**')
+            try {
+                expression.value = eval(exp)
+            } catch (EvalError){
+                expression.value = 'error'
+            }
         }
-    }
-    else if(this.value == 'C') {
-		expression.value = ''
-    }
-    else if(this.value == 'Del') {
-        expression.value = expression.value.substr(0, expression.value.length-1)
-    }
-    else {
-		expression.value += this.value
-    }
-    expression.focus()
+        else if(this.value == 'C') {
+	    expression.value = ''
+        }
+        else if(this.value == 'Del') {
+           expression.value = expression.value.substr(0, expression.value.length-1)
+        }
+        else {
+	    expression.value += this.value
+        }
+        expression.focus()
     }, false)
 }
